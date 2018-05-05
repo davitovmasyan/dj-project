4992598b2624e5dcdcc98795d74860c95a0f4243
davitovmasyan <davitovmasyan@gmail.com>
2018-05-05 22:40:43 +0400

    Configure pytest

--- apps/core/email/tests.py
+++ apps/core/email/tests.py
@@ -1,27 +1,20 @@
-from django.test import TestCase
-from django.core import mail
-
 from .utils import send_email
 
 
-class EmailTestCase(TestCase):
-    def test_send_email(self):
-        send_email(
-            subject="Test subject",
-            template_name="core/email/blank.html",
-            context={
-                "recipient_name": "Davit",
-            },
-            to="davitovmasyan@gmail.com",
-        )
+def test_send_email(mailoutbox):
+    send_email(
+        subject="Test subject",
+        template_name="core/email/blank.html",
+        context={
+            "recipient_name": "Davit",
+        },
+        to="davitovmasyan@gmail.com",
+    )
 
-        self.assertEqual(len(mail.outbox), 1)
+    assert len(mailoutbox) == 1
 
-        email = mail.outbox[0]
+    email = mailoutbox[0]
 
-        self.assertEqual(email.subject, "Test subject")
-        self.assertEqual(email.to, ["davitovmasyan@gmail.com"])
-        self.assertEqual(
-            email.body,
-            "<h1>Greetings Davit!</h1>",
-        )
+    assert email.subject == "Test subject"
+    assert email.to == ["davitovmasyan@gmail.com"]
+    assert email.body == "<h1>Greetings Davit!</h1>"
