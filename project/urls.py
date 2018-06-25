0e7b3a10b459ef41ae816d0878ef0bbb22538e26
davitovmasyan <davitovmasyan@gmail.com>
2018-06-25 15:15:21 +0400

    Add debug toolbar urls

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+if settings.ENABLE_DEBUG_TOOLBAR:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
